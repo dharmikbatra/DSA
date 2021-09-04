2cb12c73635535bfeb45bb4e0bb80fa5f29a379a
Dharmik Batra <batradharmik@gmail.com>
2021-09-04 12:20:55 +0530

    0409

---
+++ 441. Arranging Coins.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int arrangeCoins(int n) {
+        long long int N =n;
+        double k = sqrt((8*N)+1);
+        k -= 1;
+        k = k/2;
+        int k2 = k;
+        return k2;
+    }
+};
